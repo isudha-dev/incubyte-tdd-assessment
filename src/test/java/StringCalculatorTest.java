5066fb217182e2432c2232237ec94e2d5a5e1165
Sudha Maurya <sudha@Sudhas-MacBook-Pro.local>
2024-01-09 15:48:34 +0530

    added project structure and files

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
+}
